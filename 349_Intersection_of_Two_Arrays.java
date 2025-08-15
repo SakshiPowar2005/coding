b99a833b6096801ffc88b906a9b1a86c03df642d
SakshiPowar2005 <sakshipowar404@gmail.com>
2025-08-15 14:04:02 +0530

    Create 349_Intersection_of_Two_Arrays.java

---
+++ 349_Intersection_of_Two_Arrays.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+
+        int i=0;
+        int j=0;
+        int n=nums1.length;
+        int m=nums2.length;
+        int[] ans=new int[n];
+        while(i<n && j<m)
+        {
+            if(nums1[i]<nums2[j])
+            {
+                i++;
+            }
+            else if(nums2[j]<nums1[i])
+            {
+                j++;
+            }
+            else
+            {
+                ans.add(nums1[i]);
+                i++;
+                j++;
+            }
+        }
+        
+        return ans;
+    }
+}

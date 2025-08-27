4b4f2b713e6fb950a609a04b4e2391366a757f64
SakshiPowar2005 <sakshipowar404@gmail.com>
2025-08-27 12:54:50 +0530

    Create 2460_Apply_Operations_to_an_Array.java

---
+++ 2460_Apply_Operations_to_an_Array.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] applyOperations(int[] nums) {
+        
+        int [] ans = new int[nums.length];
+        for(int i = 0; i < nums.length - 1; i++)
+        {
+            if(nums[i] == nums[i + 1] && nums[i] != 0)
+            {
+                nums[i] = nums[i] * 2;
+                nums[i + 1] = 0;
+            }
+
+        }
+
+        int index = 0;
+        for(int i = 0; i < nums.length; i++)
+        {
+            if(nums[i] != 0)
+            {
+                ans[index] = nums[i];
+                index++;
+            }
+
+        }
+
+        return ans;
+
+    }
+}

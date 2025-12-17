61b6bcd6dff2223738833a3d44fac74d57b95fd7
SakshiPowar2005 <sakshipowar404@gmail.com>
2025-12-17 21:29:23 +0530

    Implement removeElements method for linked list

---
+++ 203_Remove_Linked_List_Elements.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+
+        ListNode temp = head;
+        ListNode dummynode = new ListNode(-1);
+        ListNode curr = dummynode;
+        dummynode.next = head;
+
+        while(temp != null)
+        {
+            if(temp.val == val)
+            {
+                curr.next = temp.next;
+            }
+            else
+            {
+                curr = temp;
+            }
+
+            temp = temp.next;
+        }
+
+         return dummynode.next;
+    }
+}

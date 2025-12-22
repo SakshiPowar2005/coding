29a46f002fe96142dfa4e0fa5f3a1db4a3f0ed37
SakshiPowar2005 <sakshipowar404@gmail.com>
2025-12-22 19:22:19 +0530

    Add reverse method for doubly linked list
    Implemented a method to reverse a doubly linked list.

---
+++ Reverse_a_Doubly_Linked_List.java
@@ -0,0 +1,37 @@
+/*
+class Node {
+    int data;
+    Node next;
+    Node prev;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+        this.prev = null;
+    }
+}
+*/
+class Solution {
+    public Node reverse(Node head) {
+        // code here
+        
+        Node curr = head;
+        Node temp = null;
+        
+        while(curr != null)
+        {
+            temp = curr.prev;
+            curr.prev = curr.next;
+            curr.next = temp;
+            
+            curr = curr.prev; // next pointer (<- , <-, <-)
+        }
+        
+        if(temp != null)
+        {
+            head = temp.prev; // new head
+        }
+        
+        return head;
+    }
+}

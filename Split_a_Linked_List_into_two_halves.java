ee44170d5f05d9da90465cc7eba371ee9c85b12b
SakshiPowar2005 <sakshipowar404@gmail.com>
2025-12-23 18:46:08 +0530

    Add split List method for circular linked list
    Implement a method to split a circular linked list into two halves.

---
+++ Split_a_Linked_List_into_two_halves.java
@@ -0,0 +1,41 @@
+/* Node of a linked list
+ class Node {
+   int data;
+    Node next;
+    Node(int d)  { data = d;  next = null; }
+}
+*/
+
+class Solution {
+    public Pair<Node, Node> splitList(Node head) {
+        
+        if(head == null || head.next == null)
+        {
+            return new Pair<>(head,null);
+        }
+        
+        Node slow = head;
+        Node fast = head;
+        
+        while(fast.next != head && fast.next.next != head)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        if(fast.next.next == head)
+        {
+            fast = fast.next;
+        }
+        
+        Node head1 = head;
+        Node head2 = slow.next;
+        
+        slow.next = head1;
+        fast.next = head2;
+        
+        return new Pair<>(head1, head2);
+        
+        
+    }
+}

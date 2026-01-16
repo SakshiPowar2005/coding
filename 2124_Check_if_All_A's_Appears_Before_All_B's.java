8558049fe3dd324086b30eb11876531761c12238
SakshiPowar2005 <sakshipowar404@gmail.com>
2026-01-16 21:21:41 +0530

    Implement check String to validate order of 'a's and 'b's

---
+++ 2124_Check_if_All_A's_Appears_Before_All_B's.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean checkString(String s) {
+        
+        boolean seenB = false;
+        for(int i = 0; i < s.length(); i++)
+        {
+            char ch = s.charAt(i);
+            if(ch == 'b') seenB = true;
+            if(seenB && ch == 'a') return false;
+        }
+
+        return true;
+    }
+}

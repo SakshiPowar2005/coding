10c6cc703e0c1990fb5350b8a6dabe4aeeb4bfbf
SakshiPowar2005 <sakshipowar404@gmail.com>
2026-01-17 10:32:45 +0530

    Implement max Chunks To Sorted method in Solution class

---
+++ 769_Max_Chunks_To_Make_Sorted.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxChunksToSorted(int[] arr) {
+        
+        int count = 0;
+        int maxSeen = 0;
+        for(int i = 0; i < arr.length; i++)
+        {
+            maxSeen = Math.max(maxSeen, arr[i]);
+            if(maxSeen == i)
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}

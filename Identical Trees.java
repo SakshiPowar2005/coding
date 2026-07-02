789c504149d54b4b887817d7b01da92b1b092955
SakshiPowar2005 <sakshipowar404@gmail.com>
2026-07-02 19:40:47 +0530

    Implement Check it two trees are Identical or Not.java

---
+++ Identical Trees.java
@@ -0,0 +1,22 @@
+/*
+class Node{
+    int data;
+    Node left, right;
+    Node(int d){
+        data=d;
+        left=right=null;
+    }
+}
+*/
+
+class Solution {
+    public boolean isIdentical(Node r1, Node r2) {
+       
+       if(r1 == null || r2 == null)
+       {
+           return (r1 == r2);
+       }
+       
+       return (r1.data == r2.data) && isIdentical(r1.left, r2.left) && isIdentical(r1.right, r2.right);
+    }
+}

b35cebfd64118a012dbc3ac1157accb46b020510
SakshiPowar2005 <sakshipowar404@gmail.com>
2026-07-09 23:18:19 +0530

    Implement checkTree method for binary tree validation

---
+++ 2236. Root Equals Sum of Children.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean checkTree(TreeNode root) {
+        
+        if(root == null) return true;
+        if(root.left == null && root.right == null) return true;
+
+        int child = 0;
+        if(root.left != null) child += root.left.val;
+        if(root.right != null) child += root.right.val;
+
+        return (root.val == child) && checkTree(root.left) && checkTree(root.right);
+    }
+}
